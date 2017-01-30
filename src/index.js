de14f90c237e905a2b59f09a25265c4b2d49ad3a
Marcos Souza <marcos.souza14@hotmail.com>
2017-01-30 13:52:07 -0300

    Base React

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Footer from './Footer';
+import './index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+
+ReactDOM.render(
+  <Footer />,
+  document.getElementById('footer')
+);
